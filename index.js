0deca870f24e551f95c1a63bc4b4bf48a4068b1f
nils_hackstein <nils.hackstein2@gmail.com>
2020-02-28 23:17:36 +0100

    Grundlegendes Design konzipiert

---
+++ index.js
@@ -0,0 +1,11 @@
+function deleteAll(){
+    console.log("Delete all elements.");
+}
+
+function deleteSingle(){
+    console.log("Delete single element.");
+}
+
+function divide(){
+    console.log("Divide.");
+}
